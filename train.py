768d50886a0cbbca348e22469d93e74b65108e8e
Liliia Makashova <gusmakali@student.gu.se>
2020-12-18 13:12:35 +0100

    slow model

--- train.py
+++ train.py
@@ -10,13 +10,11 @@ from encode_words import load_corpus_words, load_whole_corpus
 from models import init_model, init_opt
 # from words_batching import batch_large_data
 
-def save_checkpoint(model, opt, epoch, lr, batch_size, seq_length, path):
+def save_checkpoint(model, opt, epoch, lr, batch_size, path):
     torch.save({
         'model': model.state_dict(),
-        'tokens': model.tokens,
         'is_gru': model.is_gru,
         'bidirectional': model.bidirectional,
-        'use_embeddings': model.use_embeddings,
         'emb_dim': model.emb_dim,
         'n_hidden': model.n_hidden,
         'n_layers': model.n_layers,
@@ -25,53 +23,48 @@ def save_checkpoint(model, opt, epoch, lr, batch_size, seq_length, path):
         'opt': opt.state_dict(),
         'epoch': epoch,
         'batch_size': batch_size,
-        'seq_length': seq_length,
     }, path)
 
 def load_checkpoint(path, device):
     checkpoint = torch.load(path, map_location=torch.device(device))
-
-    model = init_model(checkpoint['tokens'], device, checkpoint['is_gru'], checkpoint['bidirectional'], checkpoint['use_embeddings'], checkpoint['emb_dim'], checkpoint['n_hidden'], checkpoint['n_layers'])
+    tokens = load_corpus_words()
+    model = init_model(tokens, device, checkpoint['is_gru'], checkpoint['bidirectional'], checkpoint['emb_dim'], checkpoint['n_hidden'], checkpoint['n_layers'])
     model.load_state_dict(checkpoint['model'])
     model.to(device)
 
     opt = init_opt(model, checkpoint['lr'])
     opt.load_state_dict(checkpoint['opt'])
 
-    return model, opt, checkpoint['epoch'], checkpoint['lr'], checkpoint['batch_size'], checkpoint['seq_length']
+    return model, opt, checkpoint['epoch'], checkpoint['lr'], checkpoint['batch_size']
 
 
 def checkpoint_path(model_name):
     return f'models/{model_name}.pt'
 
 def resume_training(device, model_name='sme_rnn', epochs=10): 
-    model, opt, starting_epoch, lr, batch_size, seq_length = load_checkpoint(checkpoint_path(model_name), device)
+    model, opt, starting_epoch, lr, batch_size = load_checkpoint(checkpoint_path(model_name), device)
 
     starting_epoch += 1
 
     print(f"\nResuming {model_name} from epoch {starting_epoch+1} ...")
 
-    run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr, batch_size, seq_length)
+    run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr, batch_size)
 
-def start_training(device, model_name='sme_rnn', epochs=10, batch_size=10, seq_length=50, lr=0.0001, bidirectional=False, use_embeddings=False, emb_dim=128, is_gru=False, n_hidden=756, n_layers=2):
-    
-    if not use_embeddings:
-        emb_dim = None
-   
+def start_training(device, model_name='sme_rnn', epochs=10, batch_size=10, lr=0.0001, bidirectional=False, emb_dim=128, is_gru=False, n_hidden=756, n_layers=2):
     
     tokens = load_corpus_words()
   
-    model = init_model(tokens, device, is_gru, bidirectional, use_embeddings, emb_dim, n_hidden, n_layers)
+    model = init_model(tokens, device, is_gru, bidirectional, emb_dim, n_hidden, n_layers)
     opt = init_opt(model, lr)
     starting_epoch = 0
 
     print(f"\nStarting to train {model_name}...")
-    run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr, batch_size, seq_length)
+    run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr, batch_size)
 
-def run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr, batch_size, seq_length):
+def run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr, batch_size):
 
     print("Model's architecture: \n", model)
-    print(f"Training with batch size: {batch_size}; seq length: {seq_length}")
+    print(f"Training with batch size: {batch_size}")
 
     clip=5
 
@@ -82,7 +75,7 @@ def run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr
     model.train()
     
     
-    x_train, y_train, x_val, y_val = load_whole_corpus()
+    x_train, x_pos_train, y_train, y_pos_train, x_val, x_pos_val, y_val, y_pos_val = load_whole_corpus()
        
 
     tokens = len(model.tokens)
@@ -90,38 +83,22 @@ def run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr
         # initialize hidden state
         h = model.init_hidden(batch_size)
       
-        for x, y in get_batch_data(x_train, y_train, batch_size):
-            x = np.asarray(x)
-            y = np.asarray(y)
-            x = x.reshape((-1, 1)) 
-            y = y.reshape((-1, 1))
-            
-            if model.use_embeddings:
-                inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
-                inputs, targets = inputs.to(device), targets.to(device)
-            else:
-                # One-hot encode, make tensor move to cuda
-                x = one_hot_encode(x, tokens)
-                inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
-                inputs, targets = inputs.to(device), targets.to(device)
-
+        for x, y in get_batch_data(x_train, x_pos_train, y_train, y_pos_train, batch_size):
+          
+            inputs, targets = x.to(device), y.to(device)
+          
             # Creating a separate variables for the hidden state
             if not model.is_gru:
                 h = tuple([each.data for each in h])
         
             model.zero_grad()
-            # print(inputs)
-            # exit()
+           
             output, h = model(inputs, h)
             # some reshaping for output needed
-            if model.bidirectional:
-                output = output.view(output.size(0), output.size(2), output.size(1))
-                loss = criterion(output, targets)
-            else:
-                loss = criterion(output, targets.view(-1).long())
+            output = output.view(output.size(0), output.size(2), output.size(1))
+            loss = criterion(output, targets)
                 
             loss.backward()
-
             nn.utils.clip_grad_norm_(model.parameters(), clip)
             opt.step()
 
@@ -132,38 +109,23 @@ def run_training_loop(model_name, model, opt, device, starting_epoch, epochs, lr
         # move to eval mode
         model.eval()
 
-        for x, y in get_batch_data(x_val. y_val, batch_size):
-            x = np.asarray(x)
-            y = np.asarray(y)
-
-            x = x.reshape((-1, 1))
-            y = y.reshape((-1, 1))
-            
-            if model.use_embeddings:
-                inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
-                inputs, targets = inputs.to(device), targets.to(device)
-            else:
-                
-                x = one_hot_encode(x, tokens)
-                inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
-                inputs, targets = inputs.to(device), targets.to(device)
-            
+        for x, y in get_batch_data(x_val, x_pos_val, y_val,y_pos_val, batch_size):
+         
+            inputs, targets = x.to(device), y.to(device)
+           
             if not model.is_gru:
                 val_h = tuple([each.data for each in val_h])
             
             output, val_h = model(inputs, val_h)
-
-            if model.bidirectional:
-                output = output.view(output.size(0), output.size(2), output.size(1))
-                val_loss = criterion(output, targets)
-            else:
-                val_loss = criterion(output, targets.view(-1).long())
             
+            output = output.view(output.size(0), output.size(2), output.size(1))
+            val_loss = criterion(output, targets)
+           
             val_losses.append(val_loss.item())
         
         model.train() # reset to train 
 
-        save_checkpoint(model, opt, e, lr, batch_size, seq_length, checkpoint_path(model_name))
+        save_checkpoint(model, opt, e, lr, batch_size, checkpoint_path(model_name))
         # return current_params
         print("Epoch: {}...".format(e+1),
             "Loss: {:.4f}...".format(loss.item()),
@@ -193,9 +155,7 @@ if __name__ == "__main__":
 
     start_parser.add_argument("--lr", type=float, help="learning rate: default 0.0001", default=0.0001)
     start_parser.add_argument("--batch-size", type=int, help="mini_batch size", required=True)
-    start_parser.add_argument("--seq-len", type=int, help="max length of a sequence to be trained on", required=True)
     start_parser.add_argument("--bidirectional", help="whether a model is bidirectonal: default False", action='store_true')
-    start_parser.add_argument("--use-emb", help="whether to ude embedding layer: default True", action='store_true')
     start_parser.add_argument("--emb-dim", type=int, help="size of emb_dim", default=128)
     start_parser.add_argument("--is-gru", help="whether to use GRU or LSTM model", action='store_true')
     start_parser.add_argument("--n-hidden", type=int, help="size of hidden layer", default=756)
@@ -212,10 +172,8 @@ if __name__ == "__main__":
             args.model_name,
             args.epochs, 
             args.batch_size,
-            args.seq_len,
             args.lr,
             args.bidirectional,
-            args.use_emb,
             args.emb_dim,
             args.is_gru,
             args.n_hidden,
@@ -223,5 +181,5 @@ if __name__ == "__main__":
         )
  
 # Example:
-# ./train.py start --device=cuda:0 --model-name {your model name} --epochs 1 --batch-size 128 --seq-len 100
+# ./train.py start --device=cuda:0 --model-name {your model name} --epochs 1 --batch-size 128 
 # ./train.py resume --device=cuda:0 --model-name {your model name} --epochs 5
